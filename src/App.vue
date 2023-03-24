730c72d2bc594d6cd5293f99464442899cec2be3
Olena Yanovska <alena.yn.adw@gmail.com>
2023-03-24 23:00:14 +0200

    added styles and functional

--- src/App.vue
+++ src/App.vue
@@ -8,12 +8,15 @@ import {
   endOfMonth, 
   startOfWeek, 
   endOfWeek, 
+  isSameDay,
 } from 'date-fns';
 
 export default {
   data() {
     return {
+      selectedDate: null,
       choosenDate: new Date(),
+      currentDate: new Date(),
     }
   },
   computed: {
@@ -21,12 +24,38 @@ export default {
       return format(this.choosenDate, 'dd');
     },
 
+    currentDayWithEnding() {
+      const day = this.currentDay;
+
+      if (day > 20 || day < 10) {
+        switch (day % 10) {
+          case 1:
+            return day + 'st'
+          case 2:
+            return day + 'nd'
+          case 3:
+            return day + 'rd'
+        }
+      }
+      return day + 'th'
+    },
+
+    currentMonth() {
+      return format(this.currentDate, 'MMMM yyyy');
+    },
+
     choosenMonth() {
       return format(this.choosenDate, 'MMMM yyyy');
     },
+
+    choosenMonth() {
+      return format(this.choosenDate, 'MMMM yyyy');
+    },
+
     daysOfWeek() {
       return ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
     },
+
     days() {
       const startOfMonthDate = startOfMonth(this.choosenDate);
       const endOfMonthDate = endOfMonth(this.choosenDate);
@@ -51,47 +80,93 @@ export default {
     nextMonth() {
       this.choosenDate = addMonths(this.choosenDate, 1);
     },
+    isToday(date) {
+      return isSameDay(date, new Date());
+    },
+    isSelected(date) {
+      return this.selectedDate && isSameDay(date, this.selectedDate);
+    },
+    select(date) {
+      this.selectedDate = date;
+    }
   }
 }
 </script>
 
 <template>
-  <div class="calendar">
-    <div class="header">
-      <button class="button-left" @click="prevMonth"></button>
-      <span>{{ choosenMonth }}</span>
-      <button class="button-right" @click="nextMonth"></button>
+  <div class="page">
+    <div class="content">
+      <p class="is-size-4">
+        Today is 
+        {{ currentDayWithEnding }} of
+        {{ currentMonth }}
+        {{ currentYear }}
+      </p>
     </div>
 
-    <div class="weekdays">
-      <span v-for="day in daysOfWeek">{{ day }}</span>
-    </div>
+    <div class="calendar">
+      <div class="has-text-light has-background-success box">
+        <div class="header">
+          <button class="button is-rounded button-left" @click="prevMonth">
+        </button>
 
-    <div class="days">
-      <span
-        v-for="(day, index) in days"
-        :key="index"
-        @click="select(day)"
-      >
-        {{ day.getDate() }}
-      </span>
+        <div class="month is-size-4">{{ choosenMonth }}</div>
+
+        <button class="button is-rounded button-right" @click="nextMonth">
+        </button>
+        </div>
+      </div>
+
+      <div class="box">
+        <div class="weekdays">
+          <span class="has-text-weight-semibold" v-for="day in daysOfWeek">{{ day }}</span>
+        </div>
+
+      <div class="days">
+        <span
+          v-for="(day, index) in days"
+          :key="index"
+          class="day has-text-centered is-size-6 is-clickable"
+          :class="{ 'has-text-light has-background-success': isToday(day), ' has-background-grey-lighter': isSelected(day) }"
+          @click="select(day)"
+        > 
+          {{ day.getDate() }}
+        </span>
+      </div>
+      </div>
     </div>
   </div>
 </template>
 
 <style>
+.page {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  height: 100%;
+}
+
+.day:hover {
+  background-color: #effaf5;
+}
+
 .calendar {
   display: flex;
   flex-direction: column;
-  width: 250px;
-  border: 1px solid black;
+  width: 350px;
 }
 
 .header {
   display: flex;
+  flex-direction: row;
+
   justify-content: space-between;
   align-items: center;
-  padding: 8px;
+}
+
+.month {
+  display: inline-block;
 }
 
 .weekdays {
@@ -103,25 +178,11 @@ export default {
 .days {
   display: grid;
   grid-template-columns: repeat(7, 1fr);
-  gap: 2px;
-  padding: 8px;
-}
-
-.today {
-  background-color:aquamarine;
-}
-
-.selected {
-  background-color: #008000;
-  color: #fff;
 }
 
 .button-left {
   background-image: url(./assets/images/arrow-left.svg);
-  width: 30px;
-  height: 30px;
-
-  border-radius: 50px;
+  display: inline-block;
 
   background-position: center;
   background-repeat: no-repeat;
@@ -129,11 +190,7 @@ export default {
 
 .button-right {
   background-image: url(./assets/images/arrow-right.svg);
-
-  width: 30px;
-  height: 30px;
-
-  border-radius: 50px;
+  display: inline-block;
 
   background-position: center;
   background-repeat: no-repeat;
--- src/main.js
+++ src/main.js
@@ -2,6 +2,6 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import 'bulma/css/bulma.css'
 
-import './assets/main.css'
+import './assets/main.scss'
 
 createApp(App).mount('#app')
